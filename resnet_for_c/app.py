ddd50b5169b46dff5bf149d33baa737cbe842676
Staziz <klesarevklesarevstas@gmail.com>
2025-06-30 22:05:35 +0400

    Update from Alex

--- resnet_for_c/app.py
+++ resnet_for_c/app.py
@@ -6,7 +6,7 @@ from utils.transforms import preprocess_image
 import os
 
 # model
-MODEL_PATH = "models/baseline_resnet18.pth"
+MODEL_PATH = "models/baseline_resnet18_new.pth"
 
 # model setup
 model = models.resnet18(pretrained=False)
@@ -15,9 +15,11 @@ model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
 model.eval()
 
 class_names = [
-    'battery', 'cardboard', 'clothes', 'food', 'glass',
+    'battery', 'biological','cardboard', 'clothes', 'glass',
     'metal', 'paper', 'plastic', 'shoes', 'trash'
 ]
+# threshhold
+CONFIDENCE_THRESHOLD = 0.8
 
 app = Flask(__name__)
 
@@ -41,15 +43,25 @@ def predict():
     # prediction
     with torch.no_grad():
         outputs = model(image_tensor)
-        _, predicted_idx = torch.max(outputs, 1)
+        # softmax - probabilities
+        probs = torch.nn.functional.softmax(outputs, dim=1)
+        max_prob, predicted_idx = torch.max(probs, dim=1)
+
+        # low probability - error 
+        if max_prob.item() < CONFIDENCE_THRESHOLD:
+            os.remove(temp_path)
+            return jsonify({"error": "low confidence", "max_prob": round(max_prob.item(), 3)}), 400
+
+        # return class if probability > threshold
         predicted_class = class_names[predicted_idx.item()]
 
     os.remove(temp_path)
 
-    return jsonify({"class": predicted_class})
+    return jsonify({"class": predicted_class, "confidence": round(max_prob.item(), 3)})
 
+# activate server
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5691)
 
 
 
--- resnet_for_c/test_request.py
+++ resnet_for_c/test_request.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 # path
-image_folder = 'test_images'
+image_folder = 'demonstration'
 url = 'http://127.0.0.1:5000/predict'
 
 # go through all images
@@ -15,9 +15,22 @@ for image_name in image_files:
         files = {'file': f}
         response = requests.post(url, files=files)
 
+
+
     print(f"\nImage: {image_name}")
     print(f"Status Code: {response.status_code}")
-    print("Prediction:", response.json())
+
+    try:
+        result = response.json()
+    except Exception as e:
+        print("❌ Error: unable to parse response as JSON.")
+        continue
+
+    # проверка: был ли возврат ошибки
+    if response.status_code == 200:
+        print(f"✅ Prediction: {result['class']} (confidence: {result['confidence']})")
+    else:
+        print(f"⚠️ Error: {result.get('error', 'Unknown error')} (confidence: {result.get('max_prob', '?')})")
 
 
 
